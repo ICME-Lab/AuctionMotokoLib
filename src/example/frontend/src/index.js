22a25558fe093fb73d31b57917565fc2f913f8b3
clankpan <clankpan26cm@gmail.com>
2022-08-19 18:32:43 +0900

    edit file arrangement

---
+++ src/example/frontend/src/index.js
@@ -0,0 +1,19 @@
+import { AuctionMotokoLib_backend } from "../../declarations/AuctionMotokoLib_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await AuctionMotokoLib_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
